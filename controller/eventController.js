08269d0bbab8391361e644fffa88ac2ac48307f7
afuentecruz <afuentecruz@gmail.com>
2016-07-06 20:18:21 +0200

    Now the events gets saved in EventRepository.js
    TODO: External filters

--- controller/eventController.js
+++ controller/eventController.js
@@ -7,6 +7,7 @@
 var mongoose = require("mongoose");
 var Event = mongoose.model('event');
 var Tag = mongoose.model('tag');
+var eventRepository = require("./../repository/EventRepository.js");
 
 var formidable = require("formidable");
 var util = require("util");
@@ -109,68 +110,20 @@ exports.postEvent = function (req, res) {
         res.write("Error: invalid JSON object");
         res.end();
     } else {
-        for (var i in fields.tagList) {
-            fields.tagList[i] = fields.tagList[i].toLowerCase();
-        }
-        var event = new Event({
-            name: fields.name,
-            latitude: fields.latitude,
-            longitude: fields.longitude,
-            //type            : "Point",
-            //loc             : [fields.latitude, fields.longitude],
-            loc: {
-                type: "Point",
-                coordinates: [fields.latitude, fields.longitude],
-                index: String
-            },
-            //location.type : "Point",
-            //location.coordinates : [fields.latitude, fields.longitude],
-            type: fields.type,
-            radius: fields.radius,
-            owner: 1,
-            tagList: fields.tagList
-            /*messageList: */
-
-        });
-
-        //Now we save (if not exists) the tags associated to the event previously saved
-        for (var i in fields.tagList) {
-            saveTagsFromEvent(fields.tagList[i]);
-        }
-        event.save(function (err) {
-            if (err != null) {
-                res.writeHead(400, {'content-type': 'text/plain'});
-                res.write("Error: " + err);
-                res.end();
-            } else {
-
+        eventRepository.saveEvent(fields, function(fields, err){
+            if(err != null){
                 res.writeHead(200, {'content-type': 'text/plain'});
                 res.write('Guardado en MongoDB : \n\n');
                 res.end(util.inspect({
                     fields: fields
                 }));
+                console.log("err not null")
+            }else{
+                res.writeHead(400, {'content-type': 'text/plain'});
+                res.write("Error: " + err);
+                res.end();
+                console.log("err is null");;
             }
         });
     }
 };
-
-/**
- * @function saveTagsFromEvent
- * @param tagName, the name of the tag that we are going to search
- * Saves a tag in mongo if already not exists
- */
-function saveTagsFromEvent(tagName) {
-
-    Tag.find({name: tagName}).count({}, function (error, numOfDocs) {
-        if (error) console.log(error);
-
-        if (numOfDocs == 0) {
-            var tag = new Tag({
-                name: tagName
-            });
-            tag.save(function (err) {
-                if (err) console.log("Error: " + err);
-            });
-        }
-    });
-}
---
+++ repository/EventRepository.js
@@ -0,0 +1,79 @@
+/**
+ * Author : Alberto de la Fuente Cruz
+ *
+ * Event model repository
+ * */
+
+var mongoose = require("mongoose");
+var Event = mongoose.model('event');
+var Tag = mongoose.model('tag');
+
+
+/*findEvent
+* saveEvent*/
+
+/**
+ * saveEvent, save an event in mongo
+ * @param obj, event params
+ * @param callback
+ */
+exports.saveEvent =  function(obj, callback){
+    if(obj != null){
+        for (var i in obj.tagList) {
+            obj.tagList[i] = obj.tagList[i].toLowerCase();
+        }
+        var event = new Event({
+            name: obj.name,
+            latitude: obj.latitude,
+            longitude: obj.longitude,
+            loc: {
+                type: "Point",
+                coordinates: [obj.latitude, obj.longitude],
+                index: String
+            },
+            type: obj.type,
+            radius: obj.radius,
+            owner: 1,
+            tagList: obj.tagList
+            /*messageList: */
+
+        });
+
+        //Now we save (if not exists) the tags associated to the event previously saved
+        for (var i in obj.tagList) {
+            saveTagsFromEvent(obj.tagList[i]);
+        }
+        event.save(function (err) {
+            if (err != null) {
+                callback(null, obj);
+            }else{
+                callback(null, obj);
+            }
+        });
+    }
+    else{
+        callback(new Error(), obj);
+    }
+};
+
+
+/**
+ * @function saveTagsFromEvent
+ * @param tagName, the name of the tag that we are going to search
+ * Saves a tag in mongo if already not exists
+ */
+function saveTagsFromEvent(tagName) {
+
+    Tag.find({name: tagName}).count({}, function (error, numOfDocs) {
+        if (error) console.log(error);
+
+        if (numOfDocs == 0) {
+            var tag = new Tag({
+                name: tagName
+            });
+            tag.save(function (err) {
+                if (err) console.log("Error: " + err);
+            });
+        }
+    });
+}
